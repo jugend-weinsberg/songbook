99481f57fc5b9213679a79008138edcebce41d14
dschklar <daniel.schklar06@gmail.com>
2025-02-11 11:00:34 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.book4',
+  appName: 'book4',
+  webDir: 'www'
+};
+
+export default config;
